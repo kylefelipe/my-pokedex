8f2493375316833817aaa55f503777a3c0563ed8
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-07-12 17:05:09 -0300

    Updates

---
+++ precache-manifest.3c58366bfdd8ef41db2650ccc36a1a8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "707d1b512294626ee21bbb1cf4ca4f0b",
+    "url": "/my-pokedex/index.html"
+  },
+  {
+    "revision": "96b7af6decc33e151175",
+    "url": "/my-pokedex/static/css/main.7daeb5d0.chunk.css"
+  },
+  {
+    "revision": "8b043fdb2f75809140f3",
+    "url": "/my-pokedex/static/js/2.814e85b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-pokedex/static/js/2.814e85b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96b7af6decc33e151175",
+    "url": "/my-pokedex/static/js/main.a33d3d4d.chunk.js"
+  },
+  {
+    "revision": "f0c54395f4d2b6baf565",
+    "url": "/my-pokedex/static/js/runtime-main.c6223624.js"
+  }
+]);
