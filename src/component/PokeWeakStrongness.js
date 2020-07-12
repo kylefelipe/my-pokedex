cc9007396dcd1362af775bfb818d3a9b40acbcde
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-07-12 20:27:09 -0300

    Criando componente PokeWeakStrongness.

---
+++ src/component/PokeWeakStrongness.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class PokeWeakStrongness extends React.Component {
+  render() {
+    return (
+      <section className="poke-weakstrong">
+        <div className="weakness-container">
+          <span>Weakness: </span>
+          <span className="weakness"> Weakness</span>
+        </div>
+        <span>"|"</span>
+        <div className="strongness-container">
+          <span>Strongness: </span>
+          <span className="strongness"> strongness</span>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default PokeWeakStrongness;
