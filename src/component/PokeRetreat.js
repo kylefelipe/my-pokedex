a257a1b43c7093c4eea9958aef506d4ac6c768bd
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-07-12 21:06:49 -0300

    Criando componente PokeRetreat.

---
+++ src/component/PokeRetreat.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class PokeRetreat extends React.Component {
+  render() {
+    return (
+      <div className="poke-retreat">
+        <span>Retreat</span>
+        <span>rrr</span>
+      </div>
+    )
+  }
+}
+
+export default PokeRetreat;
