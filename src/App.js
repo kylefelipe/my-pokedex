10a0bc53197726302f6b8e6bcea0f1214d27db6b
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-07-13 19:32:32 -0300

    Implementando o botão de próximo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Pokemon from './component/Pokemon';
 import data from './data';
 import Pokecard from './component/Pokecard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Kyle's Pokedex!
-        </p>
-        <p>
-          1st Gen!
-        </p>
-      </header>
-      <section className="pokecards-container">
-        {data.map(pokemon => <Pokecard {...pokemon}/>)}
-      </section>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super (props);
+    this.state = {filter: "fire"};
+    this.state = { pokeIndex: 0 };
+  }
+
+  setFilterValue = event => {
+    this.setState({
+      filter: event.target.value
+    })
+  }
+  
+  goToNext = event => {
+    const pokeQt = data.length;
+    this.setState((state, _) => ({
+      pokeIndex: state.pokeIndex === pokeQt - 1? 0 : state.pokeIndex + 1
+    }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Kyle's Pokedex!
+          </p>
+          <p>
+            1st Gen!
+          </p>
+        </header>
+        <section className="pokecards-container">
+          <Pokecard {...data[this.state.pokeIndex]}/>
+        </section>
+        <button 
+          className="next-pokemon"
+          key="next-pokemon"
+          onClick={this.goToNext} >
+          Next Pokemon
+        </button>
+        <button key="fire-button">Fire</button>
+        <button key="psychic-button">Psychic</button>
+      </div>
+    );
+  }
 }
 
 export default App;
